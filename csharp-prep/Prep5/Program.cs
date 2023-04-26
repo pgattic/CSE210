c23f9127cedf051a6f92df5f8836d2a39638014c
pgattic <pgattic@gmail.com>
2023-04-26 15:09:31 -0600

    Preo5 work

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+class Program {
+    
+    static void DisplayWelcome() {
+        Console.WriteLine("Ayo wassup");
+    }
+
+    static string PromptUserName() {
+        Console.Write("Enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber() {
+        Console.Write("Enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int val) {
+        return val * val;
+    }
+
+    static void DisplayResult(string name, int val) {
+        Console.WriteLine($"{name}, the sqaure of your number is {val}");
+    }
+
+    static void Main(string[] args) {
+        DisplayWelcome();
+        DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber())); // goteem
     }
 }
