292e8055c36e75b2a44920ad4bd4d5bc75949de8
pgattic <pgattic@gmail.com>
2023-04-26 14:51:22 -0600

    W02 Prep3 and 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,24 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+namespace Prep3 {
+    class Program {
+        static void Main(string[] args) {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+            int guesses = 0;
+
+            Console.WriteLine($"Random number: {number}");
+            int guess;
+            do {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guesses++;
+
+                if (guess != number) {
+                    Console.WriteLine("Incorrect, sorry");
+                }
+            } while (guess != number);
+            Console.WriteLine($"Correct! Number of guesses: {guesses}");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int nextInput;
+        List<int> words = new List<int>();
+
+        do {
+            Console.Write("Enter a number: ");
+            nextInput = int.Parse(Console.ReadLine());
+            words.Add(nextInput);
+        } while (nextInput != 0);
+
+        Console.WriteLine("Done!");
+        Console.WriteLine($"The sum is: {words.Sum()}");
+        Console.WriteLine($"The average is: {words.Average()}");
+        Console.WriteLine($"The largest number is: {words.Max()}");
     }
 }
